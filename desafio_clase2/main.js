0352f8afa006f731bcfedd0c967f0c4f230f26b6
Alejandro Duque <alejandro.duquegi@utadeo.edu.co>
2022-08-29 22:44:17 -0500

    final_commit_desafio2

---
+++ desafio_clase2/main.js
@@ -0,0 +1,42 @@
+class Usuario{
+constructor (nombre, apellido, libros, mascotas){
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.libros = libros[(nombrelibro), (autorlibro)];
+    this.mascotas = mascotas[];
+    }
+getFullName() {
+    console.log(`Buenas tardes, ``${this.nombre}`);
+}
+
+addMascota(nuevaMascota){
+    this.mascotas.push(nuevaMascota);
+}
+
+countMascotas(pets){
+    Object.keys(pets).length;
+
+}
+
+addBook([nombrelibro, autorlibro]){
+    this.addBook.push(books);
+}
+
+getBookNames(books){
+    this.books.find(books(nombrelibro));
+}
+};
+
+let usuario = new Usuario({
+    name: "Alejandro",
+    apellido: "Duque",
+    libros: [
+        "Libro 1",
+        "Libro 2",
+    ],
+    mascotas: [
+        "Ozzy",
+        "Alaska",
+    ],
+});
+   
