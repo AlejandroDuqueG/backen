eef58fcd0cf74e4e580e964fd12b9c36d46b354e
Alejandro Duque <alejandro.duquegi@utadeo.edu.co>
2022-08-31 23:10:05 -0500

    commit_clase2_correcciones

--- desafio_clase2/main.js
+++ desafio_clase2/main.js
@@ -1,42 +1,42 @@
 class Usuario{
-constructor (nombre, apellido, libros, mascotas){
+constructor (nombre, apellido, libros=[], mascotas=[]){
     this.nombre = nombre;
     this.apellido = apellido;
-    this.libros = [];
-    this.mascotas = [];
+    this.libros = libros;
+    this.mascotas = mascotas;
     }
 getFullName() {
-    console.log(`Buenas tardes, ``${this.nombre}`);
+    return `Buenas tardes,``${this.nombre}`;
+}
+    console.log(Usuario.getFullName());
 }
 
 addMascota(nuevaMascota){
     this.mascotas.push(nuevaMascota);
 }
 
-countMascotas(pets){
-    Object.keys(pets).length;
+countMascotas(){
+    console.log(this.mascotas.length);
 
 }
 
-addBook([nombrelibro, autorlibro]){
-    this.addBook.push(books);
+addBook(nombrelibro, autorlibro){
+    const books={nombrelibro, autorlibro}
+    this.libros.push(books);
 }
 
-getBookNames(books){
-    this.books.find(books(nombrelibro));
-}
+getBookNames(){
+    const libros = [{autorlibro: "Edgar Alan Poe", nombrelibro: "El gato negro"},
+                    {autorlibro: "John D. Kelleher", nombrelibro: "Data Science"}
+]
+    const nombreLibros = []
+    for (const libro of libros){
+        nombreLibros.push(Usuario.libros)
+    }
+
+    return nombreLibros
 };
 
-let usuario = new Usuario({
-    name: "Alejandro",
-    apellido: "Duque",
-    libros: [
-        "Libro 1",
-        "Libro 2",
-    ],
-    mascotas: [
-        "Ozzy",
-        "Alaska",
-    ],
-});
+let usuario = new Usuario(Alejandro, Duque,
+    [{autorlibro:"Rosa Luxemburg", nombrelibro: "Más allá del Desarrollo"}])
    
