ab2e5915de7389e9edbb8c83e8453e06a5c67817
Alejandro Duque <alejandro.duquegi@utadeo.edu.co>
2022-08-31 23:12:52 -0500

    commit_correcciones_clase2

---
+++ clase4_Manejo_Archivos_JS/main.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+const data = fs.writeFileSync('./fyh.txt', functionHora)
+console.log(data)
+
+// crea un nuevo objeto `Date`
+var functionHora = new Date();
+ 
+// obtener la fecha y la hora
+var functionHora = today.toLocaleString();
+console.log(functionHora);
